94d5c96897ca743480b0d28a10f1ec7a76b8b7eb
kevin lema <kevin.soesto@gmail.com>
2020-03-01 14:50:43 +0100

    gitignore

---
+++ src/main/java/com/gsd/gsdapi/GsdApiApplication.java
@@ -0,0 +1,13 @@
+package com.gsd.gsdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsdApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsd/gsdapi/GsdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsd.gsdapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
